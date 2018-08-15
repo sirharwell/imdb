f8bd15dc17973b64fa61b9e7e3b2c5215bc2035a
Ian Harwell <sirharwell@gmail.com>
2018-08-15 09:19:17 -0600

    actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,16 +1,56 @@
 class ActorsController < ApplicationController
-  def films
-  end
+  before_action :set_film
+  before_action :set_actor, only: [:show, :edit, :update, :destroy]
 
   def index
+    @actors = @film.actors
   end
 
   def show
   end
 
   def new
+    @actor = @film.actors.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
+
+  def create
+    @actor = @film.actors.new(actor_params)
+
+    if @actor.save
+      redirect_to [@film, @actor]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @actor.update(actor_params)
+      redirect_to [@film, @actor]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @actor.destroy
+    redirect_to film_actors_path
+  end
+
+  private
+    def set_film
+      @film = Film.find(params[:film_id])
+    end
+
+    def set_actor
+      @actor = Actor.find(params[:id])
+    end
+
+    def actor_params
+      params.require(:actor).permit(:name, :body)
+    end
 end
